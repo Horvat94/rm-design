7142a7497c1785b4ed85c57a9631903c8d72f1fa
Horvat94 <Horvat.tadej@hotmail.com>
2025-11-04 17:04:36 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+(function ($) {
+
+   // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+
+        // Back to top button
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 300) {
+                $('.back-to-top').fadeIn('slow');
+            } else {
+                $('.back-to-top').fadeOut('slow');
+            }
+        });
+        $('.back-to-top').click(function () {
+            $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+            return false;
+        });
+
+        $(".filter-button").click(function(){
+            var value = $(this).attr('data-filter');
+            
+            if(value == "all")
+            {
+                $('.filter').show('1000');
+            }
+            else
+            {
+                $(".filter").not('.'+value).hide('3000');
+                $('.filter').filter('.'+value).show('3000');
+                
+            }
+    
+                    if ($(".filter-button").removeClass("active")) {
+                $(this).removeClass("active");
+                }
+                    $(this).addClass("active");
+                });
+
+})(jQuery);
